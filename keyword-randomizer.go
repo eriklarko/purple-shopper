3e361451879e5e46ecb113cf4fa8348122445cfb
Erik Larkö <erik.larko@purplescout.se>
2014-09-14 22:26:37 +0200

    Use dictionary to generate random search keywords

--- keyword-randomizer.go
+++ keyword-randomizer.go
@@ -1,5 +1,52 @@
 package main
 
+import (
+	"os"
+	"log"
+	"bufio"
+	"time"
+	"math/rand"
+	"strings"
+)
+
+var words []string = nil
+
 func generateRandomSearchString() string {
-	return "a"
+	if words == nil {
+		setupWordsDatabase()
+	}
+	r := rand.New(rand.NewSource(time.Now().UnixNano()))
+	word := strings.ToLower(words[r.Intn(len(words))])
+
+	if r.Float64() < 0.3 {
+		word += " " + generateRandomSearchString()
+	}
+	return word
+}
+
+func setupWordsDatabase() {
+	lines, err := readLines("TWL06.txt")
+	if err != nil {
+		log.Println("Unable to open word database")
+		log.Fatal(err)
+	}
+
+	words = lines
+}
+
+// readLines reads a whole file into memory
+// and returns a slice of its lines.
+func readLines(path string) ([]string, error) {
+	file, err := os.Open(path)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+
+	var lines []string
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		lines = append(lines, scanner.Text())
+	}
+	return lines, scanner.Err()
 }
